e2718dd69a1db694cb25f39717d617b031299ac1
Anton Starichenkov <anton.starichenkov@gmail.com>
2021-08-13 16:28:43 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,19 +7,23 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select("td.altCol:nth-last-child(odd)");
-        try {
-            for (int i = 0; i < row.size(); i++) {
-                System.out.println(dates.get(i).text());
-                Element href = row.get(i).child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println();
+        String site = "https://www.sql.ru/forum/job-offers/";
+        for (int j = 1; j < 6; j++) {
+        String url = site + j;
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select("td.altCol:nth-last-child(odd)");
+            try {
+                for (int i = 0; i < row.size(); i++) {
+                    System.out.println(dates.get(i).text());
+                    Element href = row.get(i).child(0);
+                    System.out.println(href.attr("href"));
+                    System.out.println(href.text());
+                    System.out.println();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("end of elements");
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("end of elements");
         }
     }
 }
