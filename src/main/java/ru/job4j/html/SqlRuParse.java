adae9430c3cb74a415307820786c1d6c71eedb1d
Anton Starichenkov <anton.starichenkov@gmail.com>
2021-08-14 00:30:27 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,12 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         String site = "https://www.sql.ru/forum/job-offers/";
         for (int j = 1; j < 6; j++) {
-        String url = site + j;
+            String url = site + j;
             Document doc = Jsoup.connect(url).get();
             Elements row = doc.select(".postslisttopic");
             Elements dates = doc.select("td.altCol:nth-last-child(odd)");
@@ -26,4 +30,22 @@ public class SqlRuParse {
             }
         }
     }
+
+    private static void getDescriptionAndDate(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements desc = doc.select("td.msgBody");
+
+        Elements date = doc.select("td.msgFooter");
+        String d = date.get(0).text();
+        String dt = d.substring(0, d.indexOf(" ["));
+        SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
+        LocalDateTime dateTime = timeParser.parse(dt);
+    }
 }
+
+
