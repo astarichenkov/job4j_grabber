cbf8f273fe5e6d5957e24ed3045a269ef58aa0c9
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-08-14 17:41:12 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -5,66 +5,21 @@ import java.util.Objects;
 
 public class Post {
     private int id;
-    private String title;
+    private String name;
+    private String text;
     private String link;
-    private String description;
     private LocalDateTime created;
 
-    public Post(int id) {
-    }
-
-    public Post(String title, String link, String description, LocalDateTime created) {
-        this.title = title;
+    public Post(String name, String link, LocalDateTime created) {
+        this.name = name;
         this.link = link;
-        this.description = description;
         this.created = created;
     }
 
-    public Post(int id, String title, String link, String description, LocalDateTime created) {
-        this.id = id;
-        this.title = title;
+    public Post(String name, String text, String link, LocalDateTime created) {
+        this.name = name;
+        this.text = text;
         this.link = link;
-        this.description = description;
-        this.created = created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
         this.created = created;
     }
 
@@ -76,6 +31,46 @@ public class Post {
         return id == post.id;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
@@ -85,7 +80,7 @@ public class Post {
     public String toString() {
         return "Post{" +
                 "id=" + id +
-                ", title='" + title + '\'' +
+                ", title='" + name + '\'' +
                 ", created=" + created +
                 '}';
     }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,48 +4,79 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        String site = "https://www.sql.ru/forum/job-offers/";
+public class SqlRuParse implements ru.job4j.grabber.Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> postList = new ArrayList<>();
         for (int j = 1; j < 6; j++) {
-            String url = site + j;
-            Document doc = Jsoup.connect(url).get();
+            String url = link + j;
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             Elements row = doc.select(".postslisttopic");
             Elements dates = doc.select("td.altCol:nth-last-child(odd)");
             try {
                 for (int i = 0; i < row.size(); i++) {
-                    System.out.println(dates.get(i).text());
                     Element href = row.get(i).child(0);
-                    System.out.println(href.attr("href"));
-                    System.out.println(href.text());
-                    System.out.println();
+                    String postLink = href.attr("href");
+                    String postName = href.text();
+                    LocalDateTime dateTime = dateTimeParser.parse(dates.get(i).text());
+                    Post post = new Post(postName, postLink, dateTime);
+                    postList.add(post);
                 }
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("end of elements");
             }
         }
+        return postList;
     }
 
-    private static void getDescriptionAndDate(String url) {
+    @Override
+    public Post detail(String link) {
         Document doc = null;
         try {
-            doc = Jsoup.connect(url).get();
+            doc = Jsoup.connect(link).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
         Elements desc = doc.select("td.msgBody");
-
+        String text = desc.toString();
+        Elements header = doc.select("td.messageHeader");
+        String name = header.get(0).text();
         Elements date = doc.select("td.msgFooter");
         String d = date.get(0).text();
         String dt = d.substring(0, d.indexOf(" ["));
-        SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
-        LocalDateTime dateTime = timeParser.parse(dt);
+        LocalDateTime dateTime = dateTimeParser.parse(dt);
+        return new Post(name, text, link, dateTime);
+    }
+
+    public static void main(String[] args) {
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+        Post post = sqlRuParse.detail("https://www.sql.ru/forum/1336939/java-razrabotchik-moskva-udalyonka-200-300-gross");
+        System.out.println(post);
+        String link = "https://www.sql.ru/forum/job-offers/";
+        List<Post> list = sqlRuParse.list(link);
+        list.forEach(System.out::println);
     }
 }
+
 
 
